acb6406067c267781fa4bb5bd620fe4a27b42567
ivandresalin <137111909+ivandresalin@users.noreply.github.com>
2023-07-09 14:47:06 -0500

    Vehiculo completo

--- src/main/java/Clases/Vehiculo.java
+++ src/main/java/Clases/Vehiculo.java
@@ -4,10 +4,33 @@
  */
 package Clases;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author PC.1
  */
 public class Vehiculo {
+    private String placa;
     
+    public String buscarAuto(String placa){
+        try{ 
+            FileReader file1 = new FileReader("C:\\Users\\PC.1\\Desktop\\PROYECTO 1P ENTREGABLE\\vehiculos.txt");
+            BufferedReader bf1 = new BufferedReader(file1);
+            String bf2Read;
+            while((bf2Read = bf1.readLine())!=null){
+                String[] datos1 = bf2Read.split(",");
+                    if (datos1[1].equals(placa)) {
+                        return datos1[0];
+                                         
+                    }  
+                }
+            }
+            catch (IOException e){
+                System.out.println("no se encontro archivo");
+            }
+        return null;
+    }
 }
